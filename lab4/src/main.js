e60db76ef2e0653adf0c0be4683cd60b34c98a8d
PhongNguyen <phongntts01915@gmail.com>
2026-01-21 19:20:01 +0700

    Tao lab4

--- lab4/src/main.js
+++ lab4/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+
+// CHÈN 2 DÒNG BOOTSTRAP VÀO ĐÂY
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
